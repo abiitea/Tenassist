4ae12f5dadbb421fb7318a3c9e8ccc06f985d7e5
Atesfahun <ayalkbettesfahun@gmail.com>
2024-04-21 10:53:59 +0300

    update

--- Tenassist/account/views.py
+++ Tenassist/account/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, "tenassist/home.html",)
+
 def login(request):
-    return render(request, "tenassist/frontPage.html",)
+    return render(request, "tenassist/home.html",)
