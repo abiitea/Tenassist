af5543c6a50793ba1ba5c2b57cea8ce36b57e0e7
Atesfahun <ayalkbettesfahun@gmail.com>
2024-04-23 01:50:00 +0300

    update

--- Tenassist/account/urls.py
+++ Tenassist/account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name=""),
     path('home/', views.home, name=""),
     path('login/', views.login, name="login"),
+    path('register_patient/', views.register_patient, name="register_patient"),
 ]
--- Tenassist/account/views.py
+++ Tenassist/account/views.py
@@ -7,3 +7,5 @@ def login(request):
     fg = 'admin'
     rvk = request.GET.get("revoke")
     return render(request, "tenassist/login.html",)
+def register_patient(request):
+    return render(request, "tenassist/register_patient.html",)
