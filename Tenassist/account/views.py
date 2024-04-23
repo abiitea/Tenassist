b20525c3c9ded7978dba8c86bf23a83def1a07fa
Atesfahun <ayalkbettesfahun@gmail.com>
2024-04-23 17:26:05 +0300

    update

--- Tenassist/account/views.py
+++ Tenassist/account/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . forms import PatientRegistrationForm
+from django.contrib.auth.forms import UserCreationForm
 def home(request):
     return render(request, "tenassist/home.html",)
 
@@ -8,4 +9,17 @@ def login(request):
     rvk = request.GET.get("revoke")
     return render(request, "tenassist/login.html",)
 def register_patient(request):
-    return render(request, "tenassist/register_patient.html",)
+
+    form = UserCreationForm()
+    is_patient = True
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("login")
+
+    context = {'patient_form': form}
+    return render(request, "tenassist/register_patient.html", context=context)
